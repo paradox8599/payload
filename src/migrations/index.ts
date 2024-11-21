fc383af1e50299cc01d8c37a5919d9c9eae900bd
paradox8599 <paradox8599@outlook.com>
2024-11-21 17:02:07 +1100

    migrations

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20241121_055927 from './20241121_055927';
+
+export const migrations = [
+  {
+    up: migration_20241121_055927.up,
+    down: migration_20241121_055927.down,
+    name: '20241121_055927'
+  },
+];

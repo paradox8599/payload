70735726e9a349688b8448fb37833d93ea893373
paradox8599 <paradox8599@outlook.com>
2024-11-29 13:55:32 +1100

    refactor(config): conditional args

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -3,34 +3,18 @@ import sharp from 'sharp';
 import { fileURLToPath } from 'url';
 import { buildConfig, getPayload as originalGetPayload } from 'payload';
 
-import { s3Storage } from '@payloadcms/storage-s3';
 import { postgresAdapter } from '@payloadcms/db-postgres';
 import { lexicalEditor } from '@payloadcms/richtext-lexical';
-
-import { Users } from './collections/Users';
-import { Media } from './collections/Media';
 import { vercelPostgresAdapter } from '@payloadcms/db-vercel-postgres';
 import { resendAdapter } from '@payloadcms/email-resend';
 
+import { Users } from './collections/Users';
+import { Media } from './collections/Media';
+import { createS3Storage } from './payload/config/s3-storage';
+import { payloadInit } from './payload/config/init';
+
 const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
-
-if (process.env.NODE_ENV === 'development') {
-  console.log('Environment:', {
-    PAYLOAD_VERCEL: process.env.PAYLOAD_VERCEL,
-    PAYLOAD_SECRET: process.env.PAYLOAD_SECRET,
-    DATABASE_URI: process.env.DATABASE_URI,
-
-    S3_ENDPOINT: process.env.S3_ENDPOINT,
-    S3_BUCKET: process.env.S3_BUCKET,
-    S3_ACCESS_KEY_ID: process.env.S3_ACCESS_KEY_ID,
-    S3_SECRET_ACCESS_KEY: process.env.S3_SECRET_ACCESS_KEY,
-
-    RESEND_API_KEY: process.env.RESEND_API_KEY,
-    RESEND_DEFAULT_NAME: process.env.RESEND_DEFAULT_NAME,
-    RESEND_DEFAULT_FROM_ADDRESS: process.env.RESEND_DEFAULT_FROM_ADDRESS,
-  });
-}
 
 const payloadConfig = buildConfig({
   secret: process.env.PAYLOAD_SECRET!,
@@ -39,6 +23,7 @@ const payloadConfig = buildConfig({
   typescript: { outputFile: path.resolve(dirname, 'payload.d.ts') },
 
   collections: [Users, Media],
+  onInit: payloadInit,
 
   admin: {
     user: Users.slug,
@@ -64,29 +49,15 @@ const payloadConfig = buildConfig({
           },
         }),
 
-  plugins: [
-    s3Storage({
-      bucket: process.env.S3_BUCKET!,
-      config: {
-        region: 'auto',
-        endpoint: process.env.S3_ENDPOINT!,
-        credentials: {
-          accessKeyId: process.env.S3_ACCESS_KEY_ID!,
-          secretAccessKey: process.env.S3_SECRET_ACCESS_KEY!,
-        },
-      },
-      collections: {
-        media: { prefix: 'media/', disableLocalStorage: true },
-      },
-      disableLocalStorage: true,
-    }),
-  ],
+  plugins: [...createS3Storage()],
 
-  email: resendAdapter({
-    apiKey: process.env.RESEND_API_KEY!,
-    defaultFromName: process.env.RESEND_DEFAULT_NAME!,
-    defaultFromAddress: process.env.RESEND_DEFAULT_FROM_ADDRESS!,
-  }),
+  email: process.env.RESEND_API_KEY
+    ? resendAdapter({
+        apiKey: process.env.RESEND_API_KEY!,
+        defaultFromName: process.env.RESEND_DEFAULT_NAME!,
+        defaultFromAddress: process.env.RESEND_DEFAULT_FROM_ADDRESS!,
+      })
+    : undefined,
 });
 
 export async function getPayload() {

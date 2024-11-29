6c287085b5cc61ca1755f104b2524baa66ee1dcb
paradox8599 <paradox8599@outlook.com>
2024-11-30 03:18:59 +1100

    build: inherit next config for standalone build instead of maintaining a separate one

--- standalone.next.config.mjs
+++ standalone.next.config.mjs
@@ -1,7 +1,9 @@
 import { withPayload } from '@payloadcms/next/withPayload';
+import { nextConfig as appNextConfig } from './next.config.mjs';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  ...appNextConfig,
   output: 'standalone',
 };
 

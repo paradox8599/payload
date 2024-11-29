a4d2eff600cc1b1f549165a816a8861e3b09eeee
paradox8599 <paradox8599@outlook.com>
2024-11-29 21:02:51 +1100

    feat(hooks): type example

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -1,8 +1,14 @@
-import type { CollectionConfig } from 'payload';
+import { User } from '@/payload';
+import type { CollectionConfig, CollectionBeforeChangeHook } from 'payload';
 
 export const Users: CollectionConfig = {
   slug: 'users',
   admin: { useAsTitle: 'email' },
   auth: true,
   fields: [],
+  hooks: {
+    beforeChange: <CollectionBeforeChangeHook<User>[]>[
+      async ({ data }) => data,
+    ],
+  },
 };

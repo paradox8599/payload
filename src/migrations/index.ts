7cb306031d48c021b1964f27772b2ffcb7498ac3
paradox8599 <paradox8599@outlook.com>
2024-11-30 19:13:42 +1100

    feat(user): auto uuid

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20241128_050549 from './20241128_050549';
+import * as migration_20241130_081357 from './20241130_081357';
 
 export const migrations = [
   {
     up: migration_20241128_050549.up,
     down: migration_20241128_050549.down,
-    name: '20241128_050549'
+    name: '20241128_050549',
+  },
+  {
+    up: migration_20241130_081357.up,
+    down: migration_20241130_081357.down,
+    name: '20241130_081357'
   },
 ];

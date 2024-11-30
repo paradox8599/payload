5ae05d1acb80fa4227e52b464e8c9bb9c4538375
paradox8599 <paradox8599@outlook.com>
2024-11-30 19:21:11 +1100

    feat: server component with suspense

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,7 +1,14 @@
-import { getPayload } from '@/payload.config';
+import { Suspense } from 'react';
+import Users from './users';
 
 export default async function Page() {
-  const payload = await getPayload();
-  const users = await payload.count({ collection: 'users' });
-  return <main>payload: {users.totalDocs} users</main>;
+  return (
+    <main>
+      <span>payload: </span>
+      <Suspense fallback="???">
+        <Users />
+      </Suspense>
+      <span> users</span>
+    </main>
+  );
 }

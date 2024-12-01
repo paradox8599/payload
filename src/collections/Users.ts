41db4557f6160de25e53fdf95e906d2b61a07d21
paradox8599 <paradox8599@outlook.com>
2024-12-01 17:05:16 +1100

    refactor: improved uuid generator

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -1,4 +1,5 @@
 import { User } from '@/payload';
+import { PayloadFieldHook } from '@/payload.types';
 import { randomUUID } from 'crypto';
 import type { CollectionConfig, CollectionBeforeChangeHook } from 'payload';
 
@@ -14,17 +15,24 @@ export const Users: CollectionConfig = {
       hasMany: false,
       unique: true,
       access: { update: () => false },
-      admin: { readOnly: true },
-      validate: async (value, { req, operation }) => {
-        if (operation !== 'create') return true;
-        const count = await req.payload.count({
-          collection: 'users',
-          where: { uuid: { equals: value } },
-        });
-        if (count.totalDocs === 0) return true;
-        return 'UUID must be unique';
+      admin: {
+        hidden: true,
+        disableListColumn: true,
       },
-      hooks: { beforeChange: [() => randomUUID()] },
+      hooks: {
+        beforeChange: <PayloadFieldHook<User, 'uuid'>[]>[
+          async ({ req, operation }) => {
+            while (operation === 'create') {
+              const uuid = randomUUID();
+              const count = await req.payload.count({
+                collection: 'users',
+                where: { uuid: { equals: uuid } },
+              });
+              if (count.totalDocs === 0) return uuid;
+            }
+          },
+        ],
+      },
     },
   ],
   hooks: {

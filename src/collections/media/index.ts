5d5a536c666f1502f27d40b8dd55210cb2ecd1a2
paradox8599 <paradox8599@outlook.com>
2024-12-11 10:17:11 +1100

    feat(media): make relations to media display preview

--- src/collections/media/index.ts
+++ src/collections/media/index.ts
@@ -2,7 +2,9 @@ import type { CollectionConfig } from 'payload';
 
 export const Media: CollectionConfig = {
   slug: 'media',
-  upload: true,
+  upload: {
+    displayPreview: true,
+  },
   access: {
     read: () => true,
   },

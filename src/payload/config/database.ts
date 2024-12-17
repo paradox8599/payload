f066b53b8372e1204bc887ca27607b3776330c34
paradox8599 <paradox8599@outlook.com>
2024-12-17 15:24:22 +1100

    refactor(db): removed db-postgres as db-vercel-postgres supports non-vercel db now

--- src/payload/config/database.ts
+++ src/payload/config/database.ts
@@ -1,9 +1,6 @@
-import { postgresAdapter } from '@payloadcms/db-postgres';
 import { vercelPostgresAdapter } from '@payloadcms/db-vercel-postgres';
 
-export const db = (
-  process.env.VERCEL ? vercelPostgresAdapter : postgresAdapter
-)({
+export const db = vercelPostgresAdapter({
   idType: 'uuid',
   transactionOptions: { isolationLevel: undefined },
   pool: { connectionString: process.env.DATABASE_URI || undefined },
